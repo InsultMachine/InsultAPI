f991a8fbd48ca8ee9fa8bb3551e4884c48d4624a
AB <aleksandar988@gmail.com>
2017-07-02 18:21:05 +0200

    insult vote

--- Data/Models/Insult.cs
+++ Data/Models/Insult.cs
@@ -1,9 +1,12 @@
-﻿namespace Data.Models
+﻿using System.Collections.Generic;
+
+namespace Data.Models
 {
     public class Insult
     {
         public int InsultId { get; set; }
         public string Text { get; set; }
         public int Rating { get; set; }
+        public List<InsultVote> Votes { get; set; }
     }
 }
---
+++ Data/Models/InsultVote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class InsultVote
+    {
+        public int InsultVoteId { get; set; }
+        public string VoterId { get; set; }
+        public int Vote { get; set; }
+    }
+}
